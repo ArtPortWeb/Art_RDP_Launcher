4ece64170cf28584e82d9e03d1e4cca8d60171f7
MSW <m.sergio.w@gmail.com>
2026-05-16 10:01:35 +0200

    Update version.h

--- version.h
+++ version.h
@@ -3,22 +3,23 @@
 
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               0
+#define VERSION_PATCH               0
 
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
 
-// Отображаемая строка версии (только Major.Minor)
 #define VERSION_STRING \
     STRINGIZE(VERSION_MAJOR) "." \
-    STRINGIZE(VERSION_MINOR)
+    STRINGIZE(VERSION_MINOR) "." \
+    STRINGIZE(VERSION_PATCH)
 
 #define VERSION_WSTRING2(s) L##s
 #define VERSION_WSTRING(s) VERSION_WSTRING2(s)
 
 #define VERSION_STRING_W VERSION_WSTRING(VERSION_STRING)
 
-// Для ресурсов Windows (VERSIONINFO) всегда нужно 4 числа
+// Для VERSIONINFO ресурсов (требуется 4 числа)
 #define VERSION_COMMA \
-    VERSION_MAJOR, VERSION_MINOR, 0, 0
+    VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, 0
 
 #endif // VERSION_H
